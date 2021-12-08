51f29a7de5cc7201a8711431d308c91dc97cd533
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-12-08 19:24:34 +0200

    update App.js with our component

--- chapter1_1/src/App.js
+++ chapter1_1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyName extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { name: "" }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(evt) {
+    this.setState({ name: evt.target.value })
+  }
+
+  render() {
+    const { name } = this.state
+    return (<div>
+      <h1>My name is: {name}</h1>
+      <input type="text" value={name} onChange={this.handleChange} />
+    </div>)
+  }
 }
 
-export default App;
+export default MyName
