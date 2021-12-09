f18ba5b40988fe8f5a4d7795a5c550916a5178a8
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-12-09 21:45:12 +0200

    add more hooks

--- chapter3_3/src/post/CreatePost.js
+++ chapter3_3/src/post/CreatePost.js
@@ -1,24 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-function CreatePost({ user }) {
-    return (
-        <form onSubmit={e => e.preventDefault()}>
-            <div>
-                Author:<b>{user}</b>
-            </div>
-            <div>
-                <label htmlFor="create-title">Title:</label>
-                <input type="text" name="create-title" id="create-title" />
-            </div>
-            <textarea />
-            <input type="submit" value="Create" />
-        </form>
-    )
+function CreatePost({ user, posts, setPosts }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  function handleTitle(evt) {
+    setTitle(evt.target.value);
+  }
+
+  function handleContent(evt) {
+    setContent(evt.target.value);
+  }
+
+  function handleCreate() {
+    const newPost = { title, content, author: user };
+    setPosts([newPost, ...posts]);
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleCreate();
+      }}
+    >
+      <div>
+        Author:<b>{user}</b>
+      </div>
+      <div>
+        <label htmlFor="create-title">Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitle}
+          name="create-title"
+          id="create-title"
+        />
+      </div>
+      <textarea value={content} onChange={handleContent} />
+      <input type="submit" value="Create" />
+    </form>
+  );
 }
 
 CreatePost.propTypes = {
-    user: PropTypes.string
-}
+  user: PropTypes.string,
+  posts: PropTypes.array,
+  setPosts: PropTypes.func,
+};
 
-export default CreatePost
+export default CreatePost;
