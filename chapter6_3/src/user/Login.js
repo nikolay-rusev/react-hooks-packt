d7537d5b172d0fcdf868f8eb4c09a8c9b9b02f4a
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-12-20 20:14:53 +0200

    login user

--- chapter6_3/src/user/Login.js
+++ chapter6_3/src/user/Login.js
@@ -1,19 +1,45 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { StateContext } from "../contexts";
+import { useResource } from "react-request-hook";
 
 export default function Login() {
   const { dispatch } = useContext(StateContext);
   const [username, setUsername] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
+  const [password, setPassword] = useState("");
+  const [user, login] = useResource((username, password) => ({
+    url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+    method: "get",
+  }));
+
+  useEffect(() => {
+    if (user && user.data) {
+      if (user.data.length > 0) {
+        setLoginFailed(false);
+        dispatch({ type: "LOGIN", username: user.data[0].username });
+      } else {
+        setLoginFailed(true);
+      }
+    }
+
+    if (user && user.error) {
+      setLoginFailed(true);
+    }
+  }, [user]);
 
   function handleUsername(evt) {
     setUsername(evt.target.value);
+  }
+
+  function handlePassword(evt) {
+    setPassword(evt.target.value);
   }
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        dispatch({ type: "LOGIN", username });
+        login(username, password);
       }}
     >
       <label htmlFor="login-username">Username:</label>
@@ -25,8 +51,17 @@ export default function Login() {
         id="login-username"
       />
       <label htmlFor="login-password">Password:</label>
-      <input type="text" name="login-password" id="login-password" />
+      <input
+        type="password"
+        value={password}
+        onChange={handlePassword}
+        name="login-password"
+        id="login-password"
+      />
       <input type="submit" value="Login" disabled={username.length === 0} />
+      {loginFailed && (
+        <span style={{ color: "red" }}>Invalid username or password</span>
+      )}
     </form>
   );
 }
